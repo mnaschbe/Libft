c81e4ba9f3af254294027e62a4918b612f9c1635
mnaschbe <133028123+mnaschbe@users.noreply.github.com>
2023-09-25 14:07:57 +0200

    Add files via upload

---
+++ libft/ft_strndup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnaschbe <mnaschbe@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 16:52:55 by mnaschbe          #+#    #+#             */
+/*   Updated: 2023/09/25 13:31:56 by mnaschbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strndup(const char *s, size_t n)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	str = NULL;
+	if (n == 0)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (n + 1));
+	if (str == 0)
+		return (NULL);
+	while (i < n)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
